596aaecbde5407086e00ffb4a3064f085ebdbb63
shivajaini <shiva.jaini@gmail.com>
2014-07-03 14:32:29 -0700

    Initial Commit

---
+++ fb-paper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fb-paper
+//
+//  Created by Shiva Jaini on 7/3/14.
+//  Copyright (c) 2014 Shiva Jaini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
